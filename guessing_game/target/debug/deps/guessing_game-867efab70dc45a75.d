06b2e25f2db9b0728bb60354c1a24a3e896ac464
natlqs <natlqs@outlook.com>
2022-11-29 09:19:57 +0800

    a

---
+++ guessing_game/target/debug/deps/guessing_game-867efab70dc45a75.d
@@ -0,0 +1,5 @@
+e:\VMcommunicate\CODE\rust\guessing_game\target\debug\deps\guessing_game-867efab70dc45a75.rmeta: src\main.rs
+
+e:\VMcommunicate\CODE\rust\guessing_game\target\debug\deps\guessing_game-867efab70dc45a75.d: src\main.rs
+
+src\main.rs:
---
+++ guessing_game/target/debug/deps/guessing_game-ebedc8ef4c08072a.d
@@ -0,0 +1,5 @@
+e:\VMcommunicate\CODE\rust\guessing_game\target\debug\deps\guessing_game-ebedc8ef4c08072a.rmeta: src\main.rs
+
+e:\VMcommunicate\CODE\rust\guessing_game\target\debug\deps\guessing_game-ebedc8ef4c08072a.d: src\main.rs
+
+src\main.rs:
