5bc5e882e8c02081aa637b97dc75a0c1165bb732
natlqs <natlqs@outlook.com>
2024-05-07 14:48:22 +0800

    a

---
+++ z01_playground/target/debug/deps/z01_playground-22fb38f39d18230e.d
@@ -0,0 +1,5 @@
+/home/natlqs/Documents/Code/rust/z01_playground/target/debug/deps/libz01_playground-22fb38f39d18230e.rmeta: src/main.rs
+
+/home/natlqs/Documents/Code/rust/z01_playground/target/debug/deps/z01_playground-22fb38f39d18230e.d: src/main.rs
+
+src/main.rs:
